19ba86595e259b84fd07a164786dd292ec0cff9b
Lucas Henrique <lucasmonte2011@gmail.com>
2023-04-02 13:15:44 -0300

    Add script to get projects info

--- webscrapping/selenium/main.py
+++ webscrapping/selenium/main.py
@@ -15,7 +15,7 @@ driver.get('https://scratch.mit.edu/explore/projects/all')
 
 
 iteracoes = 0
-for i in range(2):
+while True:
     try:
         # Encontra todos os projetos antes de clicar no botão "carregar mais"
         projects_before = driver.find_elements(By.CLASS_NAME, 'thumbnail-image')
@@ -65,41 +65,3 @@ df.to_csv('databases/ids.csv', index=False)
         
 
 
-# for project in projects:
-#     # Aguarda 5 segundo entre cada requisição
-#     time.sleep(5)
-#     href = project.get_attribute('href')
-#     id = href.split("/")[-2]
-#     ids.append(id)
-#     url = 'https://scratchdb.lefty.one/v3/project/info/' + id
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         # A requisição foi bem-sucedida
-#         response_json = response.json()
-#         title = response_json['title']
-#         description = response_json['description']
-#         instruction = response_json['instructions']
-#         username = response_json['username']
-#         hash_id = response_json['metadata']['hash']
-
-
-#         titles.append(title)
-#         descriptions.append(description)
-#         instructions.append(instructions)
-#         username.append(username)
-#         print('Requisição bem-sucedida do projeto ' + id)
-#     else:
-#         # A requisição falhou
-#         print('Erro na requisição do projeto ' + id + ': ' + str(response.status_code) + ' ' + response.reason)
-#         titles.append(None)
-#         descriptions.append(None)
-#         instructions.append(None)
-
-
-# # Cria um DataFrame com os IDs dos projetos
-# df = pd.DataFrame({'id': ids, 'name': titles, 'description': descriptions, 'instructions': instructions})
-
-# # Imprima o DataFrame
-# df.head()
-
-# df.to_csv('ids.csv', index=False)

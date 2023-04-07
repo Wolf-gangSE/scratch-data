6f8d6dcc7aebd9686df59128242e68a58e37cf9e
Sávio Santos <savi8sant8s@gmail.com>
2023-04-06 22:36:45 -0300

    refact: updated app.py start server structure

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 
 from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from api.ProjectResource import ProjectResource
+import os
+from supabase import create_client, Client
+from flask_restful import Api
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_ANON_KEY")
+
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
@@ -12,4 +19,8 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/hello')
+
+api.add_resource(ProjectResource, '/projects', '/projects/<int:id>', resource_class_kwargs={'supabase': supabase})
+
+if __name__ == "__main__":
+    app.run(debug=True)

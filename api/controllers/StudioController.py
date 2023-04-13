8356bd02942d98142c9bb8bb8449ebd7c9065af7
Lucas Henrique <lucasmonte2011@gmail.com>
2023-04-13 19:09:50 -0300

    feat: created studio controller

---
+++ api/controllers/StudioController.py
@@ -0,0 +1,26 @@
+import supabase
+from api.services.StudioService import StudioService
+from flask_restful import reqparse
+from flask import Response
+import json
+
+
+class StudioController():
+    def fetch(self, supabase: supabase.Client, studio_id: int, offset: int):
+        count = StudioService().count_studio_projects(studio_id)
+
+        if offset >= count:
+            return Response('{"status": "error", "message": "Offset is greater than the number of projects"',
+                             status=400,
+                             content_type="application/json")
+        
+        projects = StudioService().get_studio_projects(supabase, studio_id, offset)
+
+        if projects is None:
+            return Response('{"status": "error", "message": "Failed to get studio projects"',
+                             status=500,
+                             content_type="application/json")
+        
+        return Response('{"status": "success", "message": "Studio projects fetched successfully", "projects": ' + json.dumps(projects) + ', "count": ' + str(count) + '}',
+                        status=200,
+                        content_type="application/json")

a83b91a08855a9501592dd558cbcc17872515e21
Lucas Henrique <lucasmonte2011@gmail.com>
2023-04-13 19:10:35 -0300

    feat: Add studio routes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS #comment this on deployment
 from api.ProjectResource import ProjectResource
+from api.StudioResource import StudioResource
 import os
 from supabase import create_client, Client
 from flask_restful import Api
@@ -15,12 +16,16 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
+app.config['TIMEOUT'] = 60 
+
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 
 api.add_resource(ProjectResource, '/projects', '/projects/<int:id>', resource_class_kwargs={'supabase': supabase})
+
+api.add_resource(StudioResource, '/studios/<int:id>', resource_class_kwargs={'supabase': supabase})
 
 if __name__ == "__main__":
     app.run(debug=True)

67f3467c4ea8072ee31cd8377c3950a6fdfb5c3f
Lucas Henrique <lucasmonte2011@gmail.com>
2024-07-08 07:48:16 -0300

    fix: update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://scratch-data.herokuapp.com/';
+const API_URL = 'https://scratch-data-260a21873d5c.herokuapp.com/';
 
 const api = axios.create({
   baseURL: API_URL
